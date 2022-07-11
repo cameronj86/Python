89fe4f8b10e2b25ab7c36066de171d721932172e
cameronj86 <macallik.killacam@gmail.com>
2022-07-11 13:54:50 -0500

    Completing the first 1.5 sessions

---
+++ health.py
@@ -0,0 +1,9 @@
+import random
+
+health = 50
+newhealth = potionHealth + health
+
+
+potionHealth = random.randint(25,50)
+print(potionHealth)
+print(health)
---
+++ meetingIdle.py
@@ -0,0 +1,5 @@
+firstnum = 12+4
+secondnum = 13+11
+
+print(firstnum)
+print(secondnum)
